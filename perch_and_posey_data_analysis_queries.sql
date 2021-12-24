29ce48db1d10768d63ffb56276b31b58aa0713bf
TB234 <olatunde.t.badmos@gmail.comimport osos.environ ['path'] += c:Program>
2021-12-24 11:02:55 -0500

    updated queries

--- perch_and_posey_data_analysis_queries.sql
+++ perch_and_posey_data_analysis_queries.sql
@@ -16,7 +16,7 @@ ORDER BY occurred_at ASC
 LIMIT 10;
 
 
---4 Write a query to return the top 5 orders in terms of largest total_amt_usd. 
+-- Write a query to return the top 5 orders in terms of largest total_amt_usd. 
 --Include the id, account_id, and total_amt_usd.
 SELECT id, account_id, total_amt_usd
 FROM orders
